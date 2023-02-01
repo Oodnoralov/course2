874ea233dddb5eeccd859400cd168fb0fea18cd5
Олег <hillott@yandex.ru>
2023-02-01 23:28:38 +0300

    2 задание второго курса (решение)

--- src/pro/sky/java/course2/lesson1/Main.java
+++ src/pro/sky/java/course2/lesson1/Main.java
@@ -1,14 +1,25 @@
 package pro.sky.java.course2.lesson1;
 
+import pro.sky.java.course2.lesson1.transport.Car;
+
+import java.sql.SQLOutput;
+
+
 public class Main {
     public static void main(String[] args) {
-        Car granta = new Car("Lada","Granta", 1.7, "Желтый", 2015, "Российская Федерация");
-        Car audi = new Car("Audi","A8 50 L TDI quattro",3.0,"Черный", 2020,"Германия");
-        Car bmw = new Car("BMW", "Z8", 3.0, "Черный", 2021,"Германия");
-        Car kia = new Car("KIA","sportage 4 gen",2.4, "Красный",2018, "Южная Корея");
-        Car hyundai = new Car("Hyundai","Avante", 1.6, "Оранжевый", 2016, "Южная Корея" );
+        Car granta = new Car(4,"","auto",0,true, "Lada", "Granta", 2015, "Russia", "sedan", 5);
 
-        String allCars = granta.toString() + audi.toString() + bmw.toString() + kia.toString() + hyundai.toString();
-        System.out.println(allCars);
+      granta.setEnginevalue(-2.7);
+      granta.changeTires(7);
+      granta.new Key(true, false);
+
+        System.out.println(granta.toString());
+
+
+
+
+
+
     }
+
 }
